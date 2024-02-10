d01a20ff509c966f8bc7c67798484d68849bbc26
anishshet <anishshet5@gmail.com>
2024-02-11 00:49:19 +0530

    created dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Dashboard } from "./Pages/Dashboard/Dashboard";
+import { LoginParent } from "./Pages/Login/Login";
+import Navbar from "./Pages/Navbar/Navbar";
+
 
 function App() {
   return (
-    <div className="App container text-center bg-white bg-gradient min-vw-100 min-vh-100">
-        {/* <Navbar /> */}
+    <div className="App container bg-white bg-gradient min-vw-100 min-vh-100">
+        {/* <Navbar/> */}
         <Dashboard />
-        {/* <LoginParent /> */}
+        {/* <LoginParent/> */}
     </div>
 
   );
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,28 +1,51 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import { useEffect, useState } from "react"
-import { StudentList } from "./Components/StudentList"
+import "bootstrap/dist/css/bootstrap.min.css";
+import{Chart as ChartJS} from "chart.js/auto";
+import {Bar, Line} from "react-chartjs-2";
+import { useEffect, useState } from "react";
 
 export const Dashboard = () => {
-    const divstyleCalcHeight = {
-        height: "calc(100vh - 50px)"
-    }
+    const [message, setm] = useState('');
+
+    const sendMessage = () => {
+        // Implement your sendMessage function logic here
+    };
+
+    const divStyle = {
+        height: "250px",
+        padding: "15px",
+        boxShadow: "0 3px 4px rgba(0, 0, 0, 0.1)"
+    };
 
     return (
-        // <div className="row row-col-4  min-vw-100" style={divstyleCalcHeight}>
-        //     <StudentList selectedChatId={2} UserId={3} /* receivedData={ApiData} */ />
-        // </div>
-        <div>
-
-        <div className="container d-flex" style={{ height: "50vh" }}>
-            <div className="w-50 bg-primary h-100"></div>
-            <div className="w-50 bg-dark h-100"></div>
+        <div className="row">
+            <div className="col-md-6 mb-4">
+                <div className="flex-fill" style={divStyle}>
+                    <Bar
+                    data={{
+                        labels:["A","B","C"],
+                        datasets:[{
+                            label: "Attendance",
+                            data: [60,50,60],
+                        },
+                        {
+                            label: "Absence",
+                            data: [40,50,40],
+                        },
+                    ],
+                        
+                    }}
+                    />
+                </div>
+            </div>
+            <div className="col-md-6 mb-4">
+                <div className="flex-fill" style={divStyle}>2</div>
+            </div>
+            <div className="col-md-6">
+                <div className="flex-fill" style={divStyle}>3</div>
+            </div>
+            <div className="col-md-6">
+                <div className="flex-fill" style={divStyle}>4</div>
+            </div>
         </div>
-
-        <div className="container d-flex" style={{ height: "50vh" }}>
-            <div className="w-50 bg-danger h-100"></div>
-            <div className="w-50 bg-primary h-100"></div>
-        </div>
-        </div>
-
-    )
-}
+    );
+};
