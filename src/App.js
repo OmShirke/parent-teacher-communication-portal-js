3ce991a54cd814b24242acd25c6cab1c724522c7
OmShirke <113794365+OmShirke@users.noreply.github.com>
2024-02-11 00:20:36 +0530

    studentlist

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { ChatPage } from "./Pages/Chat/Chat";
 import { Home } from "./Pages/Home/Home";
-import { LoginParent } from "./Pages/Login/Login";
+// import { LoginParent } from "./Pages/Login/Login";
 import Navbar from "./Pages/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App container text-center bg-white bg-gradient min-vw-100 min-vh-100">
-        <Navbar />
-        {/* <ChatPage /> */}
-        <Home />
+      <Navbar />
+      {/* <ChatPage /> */}
+      <Home />
     </div>
   );
 }
--- src/Pages/Home/Components/StudentList.jsx
+++ src/Pages/Home/Components/StudentList.jsx
@@ -1,19 +1,69 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { useEffect, useState } from "react"
-
+import "../styles.css"
 
 export const StudentList = () => {
-    const [message, setm] = useState('')
+    const [message, setmessage] = useState('')
     const sendMessage = () => {
 
     }
     const divStyle = {
         height: "calc(100% - 100px)"
     }
+    const [listOfNotifications, SetlistOfNotifications] = useState([{
+        id: 1,
+        data: "mesos"
+    },
+    {
+        id: 2,
+        data: "eskjf;dla"
+    },
+    {
+        id: 4,
+        data: "asjdf;kasdfj"
+    }])
+
+    const listOfNotificationsDiv = listOfNotifications.map(notification =>
+        <div key={notification.id} className="m-1 p-3 bg-dark shadow-lg rounded-5 text-white ">
+            <div className="row ">
+                <img src="blank_profile_picture.webp" className="img-fluid col-3 rounded-circle" />
+                <div className="col">
+                    {notification.data}
+                </div>
+            </div>
+        </div>)
 
     return (
-        <div className="row col m-4 p-3 bg-secondary-subtle shadow-lg rounded ">
+        // <div className="container">
+        // <div className="row col m-4 p-3 bg-secondary-subtle shadow-lg rounded ">
+        //     <div id={1} >
+        //         <div className="media-body">
+        //             <h5 class="mt-0">Center-aligned media</h5>
+        //             <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+        //             <p class="mb-0">Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+        //         </div>
+        //     </div>
 
+        // </div>
+
+        <div className="rounded d-flex ">
+            <div key={3} className="m-1 p-3 bg-dark shadow-lg rounded-5 text-white ">
+                <div className="row ">
+                    <img src="blank_profile_picture.webp" className="img-fluid col-3 rounded-circle" />
+                    <div className="col">
+                        {"notification.data"}
+                    </div>
+                </div>
+            </div>
+            <div key={3} className="m-1 p-3 bg-dark shadow-lg rounded-5 text-white ">
+                <div className="row ">
+                    <img src="blank_profile_picture.webp" className="img-fluid col-3 rounded-circle" />
+                    <div className="col">
+                        {"notification.data"}
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
